debf3fb209ef2c3db925ef01218571f89c47a412
24331a05e3-cell <24331a05e3@mvgrce.edu.in>
2026-02-20 06:40:06 +0530

    Add files via upload

---
+++ WEEK6-3.py
@@ -0,0 +1,7 @@
+#24331A05E3
+#letter'a' with letter 'x' in a string
+text = input("Enter a string: ")
+old_letter = input("Enter the letter to be replaced: ")
+new_letter = input("Enter the new letter: ")
+modified_text = text.replace(old_letter, new_letter)
+print("Modified string:", modified_text)
